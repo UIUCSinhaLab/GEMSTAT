aca1119fb15962e4791880b666f5aa207c4583f3
Bryan Lunt <lunt@ctbp.ucsd.edu>
2015-01-07 19:21:26 -0600

    Made compile on OSX

--- src/ExprPredictor.cpp
+++ src/ExprPredictor.cpp
@@ -1008,8 +1008,8 @@ double ExprFunc::compPartFuncOnQuenching() const
 {
     int n = sites.size() - 1;
     int N0 = maxContact;
-    vector< vector< double > > Z1( n + 1, N0 + 1 );
-    vector< vector< double > > Z0( n + 1, N0 + 1 );
+    Matrix Z1(n+1, N0+1);
+    Matrix Z0(n+1, N0+1);
 
     // k = 0
     for ( int i = 0; i <= n; i++ )
@@ -1019,12 +1019,12 @@ double ExprFunc::compPartFuncOnQuenching() const
         {
             if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
             bool R = testRepression( sites[j], sites[i] );
-            double term = compFactorInt( sites[ i ], sites[ j ] ) * ( Z1[j][0] + Z0[j][0] );
+            double term = compFactorInt( sites[ i ], sites[ j ] ) * ( Z1.getElement(j,0) + Z0.getElement(j,0) );
             sum1 += ( 1 - R )* term;
             sum0 += R * term;
         }
-        Z1[i][0] = bindingWts[i] * sum1;
-        Z0[i][0] = bindingWts[i] * sum0;
+	Z1.setElement(i,0, bindingWts[i] * sum1);
+	Z0.setElement(i,0, bindingWts[i] * sum0);
     }
 
     // k >= 1
@@ -1034,8 +1034,8 @@ double ExprFunc::compPartFuncOnQuenching() const
         {
             if ( i < k )
             {
-                Z1[i][k] = 0;
-                Z1[i][k] = 0;
+                Z1.setElement(i,k,0.0);
+                Z0.setElement(i,k,0.0);
                 continue;
             }
             double sum1 = 0, sum0 = 0;
@@ -1043,13 +1043,13 @@ double ExprFunc::compPartFuncOnQuenching() const
             {
                 if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
                 bool R = testRepression( sites[j], sites[i] );
-                double effect = actIndicators[sites[j].factorIdx] * ( 1 - testRepression( sites[i], sites[j] ) ) * Z1[j][k - 1] * par.txpEffects[sites[j].factorIdx];
-                double term = compFactorInt( sites[ i ], sites[ j ] ) * ( Z1[j][k] + Z0[j][k] + effect );
+                double effect = actIndicators[sites[j].factorIdx] * ( 1 - testRepression( sites[i], sites[j] ) ) * Z1.getElement(j,k-1) * par.txpEffects[sites[j].factorIdx];
+                double term = compFactorInt( sites[ i ], sites[ j ] ) * ( Z1.getElement(j,k) + Z0.getElement(j,k) + effect );
                 sum1 += ( 1 - R )* term;
                 sum0 += R * term;
             }
-            Z1[i][k] = bindingWts[i] * sum1;
-            Z0[i][k] = bindingWts[i] * sum0;
+            Z1.setElement(i,k,bindingWts[i] * sum1);
+            Z0.setElement(i,k,bindingWts[i] * sum0);
         }
     }
 
@@ -1067,12 +1067,12 @@ double ExprFunc::compPartFuncOnQuenching() const
     {
         for ( int k = 0; k <= N0; k++ )
         {
-            double term = Z1[i][k] + Z0[i][k];
+            double term = Z1.getElement(i,k) + Z0.getElement(i,k);
             Z_on += term;
         }
         for ( int k = 0; k <= N0 - 1; k++ )
         {
-            Z_on += actIndicators[sites[i].factorIdx] * Z1[i][k] * par.txpEffects[sites[i].factorIdx];
+            Z_on += actIndicators[sites[i].factorIdx] * Z1.getElement(i,k) * par.txpEffects[sites[i].factorIdx];
         }
     }
     return Z_on;
@@ -1129,17 +1129,17 @@ double ExprFunc::compPartFuncOnChrMod_Limited() const
 
     // initialization
     int N0 = maxContact;
-    vector< vector< double > > Z0( n + 1, N0 + 1 );
-    vector< vector< double > > Z1( n + 1, N0 + 1 );
-    vector< vector< double > > Zt( n + 1, N0 + 1 );
-    Z0[0][0] = 0;
-    Z1[0][0] = 0;
-    Zt[0][0] = 1.0;
+    Matrix Z0( n + 1, N0 + 1 );
+    Matrix Z1( n + 1, N0 + 1 );
+    Matrix Zt( n + 1, N0 + 1 );
+    Z0.setElement(0,0,0.0);
+    Z1.setElement(0,0,0.0);
+    Zt.setElement(0,0,1.0);
     for ( int k = 1; k <= N0; k++ )
     {
-        Z0[0][k] = 0;
-        Z1[0][k] = 0;
-        Zt[0][k] = 0;
+        Z0.setElement(0,k,0.0);
+        Z1.setElement(0,k,0.0);
+        Zt.setElement(0,k,0.0);
     }
 
     // recurrence
@@ -1148,40 +1148,43 @@ double ExprFunc::compPartFuncOnChrMod_Limited() const
         for ( int i = 1; i <= n; i++ )
         {
             //             cout << "k = " << k << " i = " << i << endl;
-            double sum0 = Zt[boundaries[i]][k], sum0A = k > 0 ? Zt[boundaries[i]][k-1] : 0, sum1 = sum0;
+            double sum0 = Zt.getElement(boundaries[i],k);
+	    double sum0A = k > 0 ? Zt.getElement(boundaries[i],k-1) : 0.0;
+	    double sum1 = sum0;
+
             for ( int j = boundaries[i] + 1; j < i; j++ )
             {
                 double dist = sites[i].start - sites[j].start;
                 if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
 
                 // sum for Z0
-                sum0 += compFactorInt( sites[i], sites[j] ) * Z0[j][k];
-                sum0A += k > 0 ? compFactorInt( sites[i], sites[j] ) * Z0[j][k-1] : 0;
+                sum0 += compFactorInt( sites[i], sites[j] ) * Z0.getElement(j,k);
+                sum0A += k > 0 ? compFactorInt( sites[i], sites[j] ) * Z0.getElement(j,k-1) : 0;
                 if ( dist > repressionDistThr )
                 {
-                    sum0 += Z1[j][k];
-                    sum0A += k > 0 ? Z1[j][k-1] : 0;
+                    sum0 += Z1.getElement(j,k);
+                    sum0A += k > 0 ? Z1.getElement(j,k-1) : 0;
                 }
 
                 // sum for Z1
                 if ( repIndicators[ sites[i].factorIdx ] )
                 {
-                    sum1 += compFactorInt( sites[i], sites[j] ) * Z1[j][k];
-                    if ( dist > repressionDistThr ) sum1 += Z0[j][k];
+                    sum1 += compFactorInt( sites[i], sites[j] ) * Z1.getElement(j,k);
+                    if ( dist > repressionDistThr ) sum1 += Z0.getElement(j,k);
                 }
             }
-            Z0[i][k] = bindingWts[i] * sum0;
-            if ( actIndicators[sites[i].factorIdx] ) Z0[i][k] += k > 0 ? bindingWts[i] * par.txpEffects[sites[i].factorIdx] * sum0A : 0;
-            if ( repIndicators[ sites[i].factorIdx ] ) Z1[i][k] = bindingWts[i] * par.repEffects[ sites[i].factorIdx ] * sum1;
-            else Z1[i][k] = 0;
-            Zt[i][k] = Z0[i][k] + Z1[i][k] + Zt[i - 1][k];
+            Z0.setElement(i,k,bindingWts[i] * sum0);
+            if ( actIndicators[sites[i].factorIdx] ) Z0(i,k) += k > 0 ? bindingWts[i] * par.txpEffects[sites[i].factorIdx] * sum0A : 0;
+            if ( repIndicators[ sites[i].factorIdx ] ) Z1(i,k) = bindingWts[i] * par.repEffects[ sites[i].factorIdx ] * sum1;
+            else Z1.setElement(i,k,0.0);
+            Zt.setElement(i,k,Z0.getElement(i,k) + Z1.getElement(i,k) + Zt.getElement(i - 1,k));
             //             cout << "i = " << i << " k = " << k << " Z0 = " << Z0[i][k] << " Z1 = " << Z1[i][k] << " Zt = " << Zt[i][k] << endl;
         }
     }
 
     // the partition function
     //     cout << "Zt[n] = " << Zt[n] << endl;
-    return sum( Zt[n] );
+    return sum( Zt.getRow(n) );//And we end up with a vector anyway. See about fixing this.
 }
 
 
