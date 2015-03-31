765331a7281af90768bc0fc3b41fe40c235c2a3e
Bryan Lunt <bjlunt2@illinois.edu>
2015-03-30 20:08:05 -0500

    Nearly done with issue #20

--- src/seq2expr.cpp
+++ src/seq2expr.cpp
@@ -355,16 +355,18 @@ int main( int argc, char* argv[] )
         axis_end.push_back( condNames.size() - 1 );
         axis_wts.push_back( 100 );
     }
-
-    //read the free fix indicator information
-    int num_indicators = 0;
-    num_indicators += nFactors + num_of_coop_pairs + nFactors; //for binding weights, coop pairs and transcriptional effects
-    num_indicators += ExprPredictor::one_qbtm_per_crm ? nSeqs : 1; //for q_btm parameter(s)
-    num_indicators += nSeqs; //for the pi parameters
-    num_indicators += nSeqs; //for the beta pars per seqs
-    num_indicators += nFactors; //for the energyThrFactors
-    num_indicators += 1; //for cic_att, duh!
-
+	
+	//read the free fix indicator information
+	int num_indicators = 0;
+	num_indicators += nFactors + num_of_coop_pairs + nFactors; //for binding weights, coop pairs and transcriptional effects
+	num_indicators += 3*nFactors; //for binding weights, and both transcriptional effects
+	num_indicators += ExprPredictor::one_qbtm_per_crm ? nSeqs : 1; //for q_btm parameter(s)
+	num_indicators += nSeqs; //for the pi parameters
+	num_indicators += nSeqs; //for the beta pars per seqs
+	num_indicators += num_of_coop_pairs; //For cooperative pairs cooperativities
+	num_indicators += nFactors; //for the energyThrFactors
+	num_indicators += 1; //for cic_att, duh!
+	
     vector <bool> indicator_bool(num_indicators, true);
     if( !free_fix_indicator_filename.empty() )
     {
