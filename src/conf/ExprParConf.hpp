ea1fc753db57dec27e63812598b7c3ec592171c0
Bryan Lunt <bjlunt2@illinois.edu>
2016-02-26 12:07:41 -0600

    centers of parameters need to be 1.0 in prob space or 0.0 in energy space for regularization to work.

--- src/conf/ExprParConf.hpp
+++ src/conf/ExprParConf.hpp
@@ -3,37 +3,36 @@
 
 ModelType ExprPar::modelOption = CHRMOD_UNLIMITED;
 SearchType ExprPar::searchOption = UNCONSTRAINED;
-int ExprPar::estBindingOption = 1;                // 1. estimate binding parameters; 0. not estimate binding parameters
+int ExprPar::estBindingOption = 1;  // 1. estimate binding parameters; 0. not estimate binding parameters
 
 double ExprPar::default_weight = 1.0;
 double ExprPar::default_interaction = 1.0;
 double ExprPar::default_effect_Logistic = 0.0;
 double ExprPar::default_effect_Thermo = 1.0;
-double ExprPar::default_repression = 1.0E-2;
+double ExprPar::default_repression = 1.0;
 double ExprPar::default_basal_Logistic = -5.0;
-double ExprPar::default_basal_Thermo = 0.01;
+double ExprPar::default_basal_Thermo = 0.001;
 double ExprPar::default_pi = 1E-50;
-double ExprPar::min_weight =0.01;
-double ExprPar::max_weight = 250.5;
-double ExprPar::min_interaction = 0.01;
-double ExprPar::max_interaction = 100.5;
+double ExprPar::min_weight = 0.0099;
+double ExprPar::max_weight = 10000.0001;
+double ExprPar::min_interaction = 0.1;
+double ExprPar::max_interaction = 1000.0001;
 double ExprPar::min_effect_Logistic = -5;
 double ExprPar::max_effect_Logistic = 5;
-// double ExprPar::min_effect_Direct = 0.01;
-double ExprPar::min_effect_Thermo = 1;
-double ExprPar::max_effect_Thermo = 4.55;
-double ExprPar::min_repression = 1E-40;
-double ExprPar::max_repression = 1;
+double ExprPar::min_effect_Thermo = 0.99;
+double ExprPar::max_effect_Thermo = 100.0001;
+double ExprPar::min_repression = 9.9E-6;
+double ExprPar::max_repression = 1.0;
 double ExprPar::min_basal_Logistic = -9.0;
 double ExprPar::max_basal_Logistic = -1.0;
-double ExprPar::min_basal_Thermo = 1.0E-5;
-double ExprPar::max_basal_Thermo = 0.105;
-double ExprPar::delta = -1.0E-75;       //We accept parameter vectors up to this far (per parameter) INSIDE the constrained space. Negative values mean slack outside the constrained interval. Positive values expose another bug that cannot be fixed immediately.
-double ExprPar::default_beta = 5;
+double ExprPar::min_basal_Thermo = 9E-4;
+double ExprPar::max_basal_Thermo = 0.010001;
+double ExprPar::delta = -1.0E-15;
+double ExprPar::default_beta = 1.0;
 double ExprPar::min_beta = 1.0E-4;
 double ExprPar::max_beta = 500;
 
-double ExprPar::min_energyThrFactors = 0.1;
+double ExprPar::min_energyThrFactors = 0.005;
 double ExprPar::max_energyThrFactors = 0.99;
 double ExprPar::default_energyThrFactors = 0.9;
 
