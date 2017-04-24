b58a58b92dca0df6423ba1aa76e5e20a8eed022e
Bryan Lunt <bjlunt2@illinois.edu>
2017-04-24 18:54:47 -0500

    Began moving out ExprFunc

---
+++ src/ExprFunc.cpp
@@ -0,0 +1,453 @@
+#include <gsl/gsl_math.h>
+
+#include "ExprPredictor.h"
+#include "ExprPar.h"
+
+
+ExprFunc::ExprFunc( const vector< Motif >& _motifs, const FactorIntFunc* _intFunc, const vector< bool >& _actIndicators, int _maxContact, const vector< bool >& _repIndicators, const IntMatrix& _repressionMat, double _repressionDistThr, const ExprPar& _par ) : motifs( _motifs ), intFunc( _intFunc ), actIndicators( _actIndicators ), maxContact( _maxContact ), repIndicators( _repIndicators ), repressionMat( _repressionMat ), repressionDistThr( _repressionDistThr )
+{
+    par = _par;
+
+    int nFactors = par.nFactors();
+    assert( motifs.size() == nFactors );
+    assert( actIndicators.size() == nFactors );
+    assert( repIndicators.size() == nFactors );
+    assert( repressionMat.isSquare() && repressionMat.nRows() == nFactors );
+    assert( maxContact >= 0 );
+
+}
+
+
+double ExprFunc::predictExpr( const SiteVec& _sites, int length, const vector< double >& factorConcs, int seq_num )
+{
+    bindingWts.clear(); boundaries.clear();
+
+    if( !one_qbtm_per_crm )
+        seq_num = 0;
+    // store the sequence
+    int n = _sites.size();
+    sites = SiteVec(_sites);
+    sites.insert( sites.begin(), Site() );        // start with a pseudo-site at position 0
+    boundaries.push_back( 0 );
+    double range = max( intFunc->getMaxDist(), repressionDistThr );
+    for ( int i = 1; i <= n; i++ )
+    {
+        int j;
+        for ( j = i - 1; j >= 1; j-- )
+        {
+            if ( ( sites[i].start - sites[j].start ) > range ) break;
+        }
+        int boundary = j;
+        boundaries.push_back( boundary );
+    }
+
+    // compute the Boltzman weights of binding for all sites
+    bindingWts.push_back( 1.0 );
+    for ( int i = 1; i <= n; i++ )
+    {
+        bindingWts.push_back( par.maxBindingWts[ sites[i].factorIdx ] * factorConcs[sites[i].factorIdx] * sites[i].prior_probability * sites[i].wtRatio );
+        double samee = par.maxBindingWts[sites[i].factorIdx]*factorConcs[sites[i].factorIdx]*sites[i].prior_probability*sites[i].wtRatio;
+        if(samee != samee)
+        {
+            cout << "DEBUG: samee for " << i << "\t" << sites[i].factorIdx << "\t" <<  par.maxBindingWts[sites[i].factorIdx] <<"\t" << factorConcs[sites[i].factorIdx] <<"\t" << sites[i].prior_probability <<"\t" << sites[i].wtRatio << endl;
+            exit(1);
+        }
+    }
+
+    // Logistic model
+    if ( modelOption == LOGISTIC )
+    {
+                                                  // total occupancy of each factor
+        vector< double > factorOcc( motifs.size(), 0 );
+        for ( int i = 1; i < sites.size(); i++ )
+        {
+            factorOcc[ sites[i].factorIdx ] += bindingWts[i] / ( 1.0 + bindingWts[i] );
+        }
+        double totalEffect = 0;
+        //         cout << "factor\toccupancy\ttxp_effect" << endl;
+        for ( int i = 0; i < motifs.size(); i++ )
+        {
+            double effect = par.txpEffects[i] * factorOcc[i];
+            totalEffect += effect;
+            //             cout << i << "\t" << factorOcc[i] << "\t" << effect << endl;
+
+            // length correction
+            //             totalEffect = totalEffect / (double)length;
+        }
+        //         return par.expRatio * logistic( log( par.basalTxp ) + totalEffect );
+        return logistic( par.basalTxps[ seq_num ] + totalEffect );
+    }
+
+    // Thermodynamic models: Direct, Quenching, ChrMod_Unlimited and ChrMod_Limited
+    // compute the partition functions
+    double Z_off = compPartFuncOff();
+    //cout << "Z_off = " << Z_off << endl;
+    double Z_on = compPartFuncOn();
+    //cout << "Z_on = " << Z_on << endl;
+
+    // compute the expression (promoter occupancy)
+    double efficiency = Z_on / Z_off;
+    //cout << "efficiency = " << efficiency << endl;
+    //cout << "basalTxp = " << par.basalTxps[ seq_num ] << endl;
+    double promoterOcc = efficiency * par.basalTxps[ seq_num ] / ( 1.0 + efficiency * par.basalTxps[ seq_num ] /** ( 1 + par.pis[ seq_num ] )*/ );
+    return promoterOcc;
+}
+
+ModelType ExprFunc::modelOption = QUENCHING;
+
+double ExprFunc::compPartFuncOff() const
+{
+    if ( modelOption == CHRMOD_UNLIMITED || modelOption == CHRMOD_LIMITED ) return compPartFuncOffChrMod();
+
+    int n = sites.size() - 1;
+    // initialization
+    vector< double > Z( n + 1 );
+    Z[0] = 1.0;
+    vector< double > Zt( n + 1 );
+    Zt[0] = 1.0;
+
+    // recurrence
+    for ( int i = 1; i <= n; i++ )
+    {
+        double sum = Zt[boundaries[i]];
+        if( sum != sum )
+        {
+            cout << "DEBUG: sum nan" << "\t" << Zt[ boundaries[i] ] <<  endl;
+            exit(1);
+        }
+        //cout << "DEBUG: sum = " << n << endl;
+        for ( int j = boundaries[i] + 1; j < i; j++ )
+        {
+            if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+            //cout << "compFactorInt: " << compFactorInt( sites[ i ], sites[ j ] ) << "\t";
+            //cout << "Z[j]: " << Z[ j ] << endl;
+            double old_sum = sum;
+            sum += compFactorInt( sites[ i ], sites[ j ] ) * Z[ j ];
+            if( sum != sum || isinf( sum ))
+            {
+                cout << "Old sum:\t" << old_sum << endl;
+                cout << "Factors:\t" << sites[ i ].factorIdx << "\t" << sites[ j ].factorIdx << endl;
+                cout << "compFactorInt:\t" << compFactorInt( sites[ i ], sites[ j ] ) << endl;
+                cout << "Z[j]:\t" << Z[ j ] << endl;
+                cout << i << "\t" << j << "\t" << par.factorIntMat( (sites[i]).factorIdx, (sites[j]).factorIdx ) << endl;
+                cout << "DEBUG: sum nan/inf\t"<< sum << endl;
+                exit(1);
+            }
+        }
+
+        Z[i] = bindingWts[ i ] * sum;
+        if( Z[i]!=Z[i] )
+        {
+            cout << "DEBUG: Z bindingWts[i]: " << sites[i].factorIdx << "\t" << bindingWts[ sites[i].factorIdx ] <<"\t" << sum << endl;
+            exit(1);
+        }
+        Zt[i] = Z[i] + Zt[i - 1];
+        //cout << "debug: Zt[i] = " << Zt[i] << endl;
+    }
+
+    // the partition function
+    // 	double Z_bind = 1;
+    // 	for ( int i = 0; i < sites.size(); i++ ) {
+    // 		Z_bind += Z[ i ];
+    // 	}
+    return Zt[n];
+}
+
+
+double ExprFunc::compPartFuncOffChrMod() const
+{
+    int n = sites.size()- 1;
+
+    // initialization
+    vector< double > Z0( n + 1 );
+    Z0[0] = 1.0;
+    vector< double > Z1( n + 1 );
+    Z1[0] = 1.0;
+    vector< double > Zt( n + 1 );
+    Zt[0] = 1.0;
+
+    // recurrence
+    for ( int i = 1; i <= n; i++ )
+    {
+        double sum = Zt[boundaries[i]];
+        double sum0 = sum, sum1 = sum;
+        for ( int j = boundaries[i] + 1; j < i; j++ )
+        {
+            if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+            double dist = sites[i].start - sites[j].start;
+
+            // sum for Z0
+            sum0 += compFactorInt( sites[i], sites[j] ) * Z0[j];
+            if ( dist > repressionDistThr ) sum0 += Z1[j];
+
+            // sum for Z1
+            if ( repIndicators[ sites[i].factorIdx ] )
+            {
+                sum1 += compFactorInt( sites[i], sites[j] ) * Z1[j];
+                if ( dist > repressionDistThr ) sum1 += Z0[j];
+            }
+        }
+        Z0[i] = bindingWts[i] * sum0;
+        if ( repIndicators[ sites[i].factorIdx ] ) Z1[i] = bindingWts[i] * par.repEffects[ sites[i].factorIdx ] * sum1;
+        else Z1[i] = 0;
+        Zt[i] = Z0[i] + Z1[i] + Zt[i - 1];
+    }
+
+    // the partition function
+    return Zt[n];
+}
+
+
+double ExprFunc::compPartFuncOn() const
+{
+    if ( modelOption == DIRECT ) return compPartFuncOnDirect();
+    if ( modelOption == QUENCHING ) return compPartFuncOnQuenching();
+    if ( modelOption == CHRMOD_UNLIMITED) return compPartFuncOnChrMod_Unlimited();
+    if ( modelOption == CHRMOD_LIMITED ) return compPartFuncOnChrMod_Limited();
+//TODO: A compiler warning is generated here. Shouldn't there be some defensive coding?
+}
+
+
+double ExprFunc::compPartFuncOnDirect() const
+{
+    int n = sites.size() - 1;
+
+    // initialization
+    vector< double > Z( n + 1 );
+    Z[0] = 1.0;
+    vector< double > Zt( n + 1 );
+    Zt[0] = 1.0;
+
+    // recurrence
+    for ( int i = 1; i <= n; i++ )
+    {
+        double sum = Zt[boundaries[i]];
+        for ( int j = boundaries[i] + 1; j < i; j++ )
+        {
+            if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+            sum += compFactorInt( sites[ i ], sites[ j ] ) * Z[ j ];
+        }
+        //Z[i] = bindingWts[ i ] * par.txpEffects[ sites[i].factorIdx ] * sum;
+        if( actIndicators[ sites[ i ].factorIdx ] )
+        {
+            Z[ i ] = bindingWts[ i ] * par.txpEffects[ sites[ i ].factorIdx ] * sum;
+            //cout << "1: " << par.txpEffects[ sites[ i ].factorIdx ] << endl;
+        }
+        if( repIndicators[ sites[ i ].factorIdx ] )
+        {
+            Z[ i ] = bindingWts[ i ] * par.repEffects[ sites[ i ].factorIdx ] * sum;
+            //cout << "2: " << par.repEffects[ sites[ i ].factorIdx ] << endl;
+        }
+        //cout << "DEBUG 0: " << sum << "\t" << Zt[ i - 1] << endl;
+        Zt[i] = Z[i] + Zt[i - 1];
+        /*if( actIndicators[ sites[ i ].factorIdx ] )
+            cout << "DEBUG 1: " << Zt[i] << "\t" << bindingWts[i]*par.txpEffects[sites[i].factorIdx]*(Zt[ i - 1] + 1) << endl;
+        if( repIndicators[ sites[ i ].factorIdx ] )
+            cout << "DEBUG 2: " << Zt[i] << "\t" << bindingWts[i]*par.repEffects[sites[i].factorIdx]*(Zt[ i - 1] + 1) << endl;*/
+    }
+
+    return Zt[n];
+}
+
+
+double ExprFunc::compPartFuncOnQuenching() const
+{
+    int n = sites.size() - 1;
+    int N0 = maxContact;
+    Matrix Z1(n+1, N0+1);
+    Matrix Z0(n+1, N0+1);
+
+    // k = 0
+    for ( int i = 0; i <= n; i++ )
+    {
+        double sum1 = 1, sum0 = 0;
+        for ( int j = 1; j < i; j++ )
+        {
+            if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+            bool R = testRepression( sites[j], sites[i] );
+            double term = compFactorInt( sites[ i ], sites[ j ] ) * ( Z1.getElement(j,0) + Z0.getElement(j,0) );
+            sum1 += ( 1 - R )* term;
+            sum0 += R * term;
+        }
+	Z1.setElement(i,0, bindingWts[i] * sum1);
+	Z0.setElement(i,0, bindingWts[i] * sum0);
+    }
+
+    // k >= 1
+    for ( int k = 1; k <= N0; k++ )
+    {
+        for ( int i = 0; i <= n; i++ )
+        {
+            if ( i < k )
+            {
+                Z1.setElement(i,k,0.0);
+                Z0.setElement(i,k,0.0);
+                continue;
+            }
+            double sum1 = 0, sum0 = 0;
+            for ( int j = 1; j < i; j++ )
+            {
+                if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+                bool R = testRepression( sites[j], sites[i] );
+                double effect = actIndicators[sites[j].factorIdx] * ( 1 - testRepression( sites[i], sites[j] ) ) * Z1.getElement(j,k-1) * par.txpEffects[sites[j].factorIdx];
+                double term = compFactorInt( sites[ i ], sites[ j ] ) * ( Z1.getElement(j,k) + Z0.getElement(j,k) + effect );
+                sum1 += ( 1 - R )* term;
+                sum0 += R * term;
+            }
+            Z1.setElement(i,k,bindingWts[i] * sum1);
+            Z0.setElement(i,k,bindingWts[i] * sum0);
+        }
+    }
+
+    //     for ( int i = 1; i <= n; i++ ) {
+    //         for ( int k = 0; k <= N0; k++ ) {
+    //             cout << "Z1(" << i << ", " << k << ") = " << Z1[i][k] << "\t";
+    //             cout << "Z0(" << i << ", " << k << ") = " << Z0[i][k] << endl;
+    //         }
+    //         cout << endl;
+    //     }
+
+    // the partition function
+    double Z_on = 1;
+    for ( int i = 1; i <= n; i++ )
+    {
+        for ( int k = 0; k <= N0; k++ )
+        {
+            double term = Z1.getElement(i,k) + Z0.getElement(i,k);
+            Z_on += term;
+        }
+        for ( int k = 0; k <= N0 - 1; k++ )
+        {
+            Z_on += actIndicators[sites[i].factorIdx] * Z1.getElement(i,k) * par.txpEffects[sites[i].factorIdx];
+        }
+    }
+    return Z_on;
+}
+
+
+double ExprFunc::compPartFuncOnChrMod_Unlimited() const
+{
+    int n = sites.size()- 1;
+
+    // initialization
+    vector< double > Z0( n + 1 );
+    Z0[0] = 1.0;
+    vector< double > Z1( n + 1 );
+    Z1[0] = 1.0;
+    vector< double > Zt( n + 1 );
+    Zt[0] = 1.0;
+
+    // recurrence
+    for ( int i = 1; i <= n; i++ )
+    {
+        double sum = Zt[boundaries[i]];
+        double sum0 = sum, sum1 = sum;
+        for ( int j = boundaries[i] + 1; j < i; j++ )
+        {
+            double dist = sites[i].start - sites[j].start;
+            if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+
+            // sum for Z0
+            sum0 += compFactorInt( sites[i], sites[j] ) * Z0[j];
+            if ( dist > repressionDistThr ) sum0 += Z1[j];
+
+            // sum for Z1
+            if ( repIndicators[ sites[i].factorIdx ] )
+            {
+                sum1 += compFactorInt( sites[i], sites[j] ) * Z1[j];
+                if ( dist > repressionDistThr ) sum1 += Z0[j];
+            }
+        }
+        Z0[i] = bindingWts[i] * par.txpEffects[ sites[i].factorIdx ] * sum0;
+        if ( repIndicators[ sites[i].factorIdx ] ) Z1[i] = bindingWts[i] * par.repEffects[ sites[i].factorIdx ] * sum1;
+        else Z1[i] = 0;
+        Zt[i] = Z0[i] + Z1[i] + Zt[i - 1];
+    }
+
+    // the partition function
+    return Zt[n];
+}
+
+
+double ExprFunc::compPartFuncOnChrMod_Limited() const
+{
+    int n = sites.size()- 1;
+
+    // initialization
+    int N0 = maxContact;
+    Matrix Z0( n + 1, N0 + 1 );
+    Matrix Z1( n + 1, N0 + 1 );
+    Matrix Zt( n + 1, N0 + 1 );
+    Z0.setElement(0,0,0.0);
+    Z1.setElement(0,0,0.0);
+    Zt.setElement(0,0,1.0);
+    for ( int k = 1; k <= N0; k++ )
+    {
+        Z0.setElement(0,k,0.0);
+        Z1.setElement(0,k,0.0);
+        Zt.setElement(0,k,0.0);
+    }
+
+    // recurrence
+    for ( int k = 0; k <= N0; k++ )
+    {
+        for ( int i = 1; i <= n; i++ )
+        {
+            //             cout << "k = " << k << " i = " << i << endl;
+            double sum0 = Zt.getElement(boundaries[i],k);
+	    double sum0A = k > 0 ? Zt.getElement(boundaries[i],k-1) : 0.0;
+	    double sum1 = sum0;
+
+            for ( int j = boundaries[i] + 1; j < i; j++ )
+            {
+                double dist = sites[i].start - sites[j].start;
+                if ( siteOverlap( sites[ i ], sites[ j ], motifs ) ) continue;
+
+                // sum for Z0
+                sum0 += compFactorInt( sites[i], sites[j] ) * Z0.getElement(j,k);
+                sum0A += k > 0 ? compFactorInt( sites[i], sites[j] ) * Z0.getElement(j,k-1) : 0;
+                if ( dist > repressionDistThr )
+                {
+                    sum0 += Z1.getElement(j,k);
+                    sum0A += k > 0 ? Z1.getElement(j,k-1) : 0;
+                }
+
+                // sum for Z1
+                if ( repIndicators[ sites[i].factorIdx ] )
+                {
+                    sum1 += compFactorInt( sites[i], sites[j] ) * Z1.getElement(j,k);
+                    if ( dist > repressionDistThr ) sum1 += Z0.getElement(j,k);
+                }
+            }
+            Z0.setElement(i,k,bindingWts[i] * sum0);
+            if ( actIndicators[sites[i].factorIdx] ) Z0(i,k) += k > 0 ? bindingWts[i] * par.txpEffects[sites[i].factorIdx] * sum0A : 0;
+            if ( repIndicators[ sites[i].factorIdx ] ) Z1(i,k) = bindingWts[i] * par.repEffects[ sites[i].factorIdx ] * sum1;
+            else Z1.setElement(i,k,0.0);
+            Zt.setElement(i,k,Z0.getElement(i,k) + Z1.getElement(i,k) + Zt.getElement(i - 1,k));
+            //             cout << "i = " << i << " k = " << k << " Z0 = " << Z0[i][k] << " Z1 = " << Z1[i][k] << " Zt = " << Zt[i][k] << endl;
+        }
+    }
+
+    // the partition function
+    //     cout << "Zt[n] = " << Zt[n] << endl;
+    return sum( Zt.getRow(n) );//And we end up with a vector anyway. See about fixing this.
+}
+
+
+double ExprFunc::compFactorInt( const Site& a, const Site& b ) const
+{
+    // 	assert( !siteOverlap( a, b, motifs ) );
+    double maxInt = par.factorIntMat( a.factorIdx, b.factorIdx );
+    double dist = abs( a.start - b.start );
+    bool orientation = ( a.strand == b.strand );
+    return intFunc->compFactorInt( maxInt, dist, orientation );
+}
+
+
+bool ExprFunc::testRepression( const Site& a, const Site& b ) const
+{
+    // 	assert( !siteOverlap( a, b, motifs ) );
+
+    double dist = abs( a.start - b.start );
+    return repressionMat( a.factorIdx, b.factorIdx ) && ( dist <= repressionDistThr );
+}
---
+++ src/ExprFunc.h
@@ -0,0 +1,85 @@
+#ifndef EXPR_FUNC_H
+#define EXPR_FUNC_H
+
+#include "ExprModel.h"
+#include "FactorIntFunc.h"
+#include "ExprPar.h"
+
+/*****************************************************
+ * Expression Model and Parameters
+ ******************************************************/
+
+
+/* ExprFunc class: predict the expression (promoter occupancy) of an enhancer sequence */
+class ExprFunc
+{
+    public:
+        // constructors
+        ExprFunc( const vector< Motif >& _motifs, const FactorIntFunc* _intFunc, const vector< bool >& _actIndicators, int _maxContact, const vector< bool >& _repIndicators, const IntMatrix& _repressionMat, double _repressionDistThr, const ExprPar& _par );
+
+        // access methods
+        const vector< Motif >& getMotifs() const
+        {
+            return motifs;
+        }
+
+        // predict the expression value of a given sequence (its site representation, sorted by the start positions) under given TF concentrations
+        double predictExpr( const SiteVec& _sites, int length, const vector< double >& factorConcs, int seq_num );
+        const ExprPar& getPar() const { return par; }
+
+        static ModelType modelOption;             // model option
+        static bool one_qbtm_per_crm;
+    private:
+        // TF binding motifs
+        const vector< Motif >& motifs;
+
+        // control parameters
+        const FactorIntFunc* intFunc;             // function to compute distance-dependent TF-TF interactions
+        const vector< bool >& actIndicators;      // 1 if the TF is in the activator set
+        int maxContact;                           // the maximum contact
+        const vector< bool >& repIndicators;      // 1 if the TF is in the repressor set
+        const IntMatrix& repressionMat;           // repression matrix: R(f,f') = 1 if f can repress f'
+        double repressionDistThr;                 // distance threshold for repression: d_R
+
+        // model parameters
+        ExprPar par;//NOTE: Removing "const" here caused the copy constructor to be called. Thus the ExprFunc gets its own copy that will not have problems when the original par is changed.
+
+        // the sequence whose expression is to be predicted
+        SiteVec sites;
+        vector< int > boundaries;                 // left boundary of each site beyond which there is no interaction
+
+        // intermediate computational results
+        vector< double > bindingWts;
+
+        // compute the partition function when the basal transcriptional machinery (BTM) is not bound
+        double compPartFuncOff() const;
+
+        // compute the partition function when the BTM is not bound: ChrMod model
+        double compPartFuncOffChrMod() const;
+
+        // compute the partition function when the BTM is bound
+        double compPartFuncOn() const;
+
+        // compute the paritition function when the BTM is bound: Direct model
+        double compPartFuncOnDirect() const;
+
+        // compute the paritition function when the BTM is bound: Quenching model
+        double compPartFuncOnQuenching() const;
+
+        // compute the paritition function when the BTM is bound: ChrMod_Unlimited model
+        double compPartFuncOnChrMod_Unlimited() const;
+
+        // compute the paritition function when the BTM is bound: ChrMod_Limited model
+        double compPartFuncOnChrMod_Limited() const;
+
+        // compute the TF-TF interaction between two occupied sites
+        double compFactorInt( const Site& a, const Site& b ) const;
+
+        double compDen() const;
+        double compNum( int TFid ) const;
+
+        // test if one site represses another site
+        bool testRepression( const Site& a, const Site& b ) const;
+};
+
+#endif
