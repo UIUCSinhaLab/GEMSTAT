a56caa49be6a21c6264b75e0ff00d2bbdd7dc275
Bryan Lunt <bjlunt2@illinois.edu>
2017-06-09 22:42:03 +0000

    progress

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 aclocal -I m4 --install
 autoconf
 automake --add-missing
-./configure --with-beta-optimize=together
+#./configure --with-beta-optimize=together
+bash external/buildexternal.bash
+PATH=${PATH}:./external/usr/bin/ ./configure --with-beta-optimize=together --with-gsl-prefix=external/usr/ --with-nlopt-prefix=external/usr/
 make
--- external/buildexternal.bash
+++ external/buildexternal.bash
@@ -17,3 +17,7 @@ mkdir -p usr
 curl -O "http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz"
 tar -zxvof nlopt-2.4.2.tar.gz
 (cd nlopt-2.4.2 ; ./configure --without-guile --without-python --without-octave --without-matlab --with-cxx --prefix=${SCRIPT_DIR}/usr; make ; make install)
+
+curl -O "http://reflection.oss.ou.edu/gnu/gsl/gsl-2.3.tar.gz"
+tar -zxvof gsl-2.3.tar.gz
+(cd gsl-2.3 ; ./configure --enable-static=yes --enable-shared=no --prefix=${SCRIPT_DIR}/usr/ ; make ; make install)
