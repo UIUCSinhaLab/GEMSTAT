645506f5147f8cafb79dafcb2e3a5ba7371dc6f1
Bryan Lunt <bjlunt2@illinois.edu>
2017-06-13 02:44:01 -0500

    Altered autogen to ensure that fetched external libraries are preferentially used.

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,6 @@ autoconf
 automake --add-missing
 #./configure --with-beta-optimize=together
 bash external/buildexternal.bash
-PATH=${PATH}:./external/usr/bin/ ./configure --with-beta-optimize=together --with-gsl-prefix=external/usr/ --with-nlopt-prefix=external/usr/
+PATH=${PWD}/external/usr/bin/:${PATH} ./configure --with-beta-optimize=together --with-gsl-prefix=${PWD}/external/usr/ --with-nlopt-prefix=${PWD}/external/usr/
+make clean
 make
