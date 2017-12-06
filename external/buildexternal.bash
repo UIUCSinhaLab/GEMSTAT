927b7d7f07b2d71fc817e108e6f41daa17a71d80
Bryan Lunt <bjlunt2@illinois.edu>
2017-12-06 11:14:30 -0600

    Fetch external code even if redirected.

--- external/buildexternal.bash
+++ external/buildexternal.bash
@@ -14,10 +14,10 @@ cd ${SCRIPT_DIR}
 
 mkdir -p usr
 
-curl -O "http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz"
+curl -L -O "http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz"
 tar -zxvof nlopt-2.4.2.tar.gz
 (cd nlopt-2.4.2 ; ./configure --without-guile --without-python --without-octave --without-matlab --with-cxx --prefix=${SCRIPT_DIR}/usr; make ; make install)
 
-curl -O "http://reflection.oss.ou.edu/gnu/gsl/gsl-2.3.tar.gz"
-tar -zxvof gsl-2.3.tar.gz
-(cd gsl-2.3 ; ./configure --enable-static=yes --enable-shared=no --prefix=${SCRIPT_DIR}/usr/ ; make ; make install)
+curl -L -O "http://reflection.oss.ou.edu/gnu/gsl/gsl-2.4.tar.gz"
+tar -zxvof gsl-2.4.tar.gz
+(cd gsl-2.4 ; ./configure --enable-static=yes --enable-shared=no --prefix=${SCRIPT_DIR}/usr/ ; make ; make install)
