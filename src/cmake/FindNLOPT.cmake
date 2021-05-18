7cb111def24b2c0f71f135ce93e1d00101f595c4
Bryan Lunt <bjlunt2@illinois.edu>
2021-05-18 18:57:30 -0500

    nlopt as submodule

--- src/cmake/FindNLOPT.cmake
+++ src/cmake/FindNLOPT.cmake
@@ -1,4 +1,23 @@
 #stuff to find and download/check out nlopt or find one installed on the system.
 
 
+#include(ExternalProject)
+#ExternalProject_Add(nlopt_project
+#  GIT_REPOSITORY https://github.com/stevengj/nlopt.git
+#  STEP_TARGETS   build
+#  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/nlopt
+#  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+#)
+#ExternalProject_Get_Property(nlopt_project install_dir)
+#include_directories(${install_dir}/include)
+
+#message("NLOPT LIBS ")
+
+#include(ExternalProject)
+#ExternalProject_Add(foobar
+#  GIT_REPOSITORY https://github.com/stevengj/nlopt.git
+#  GIT_TAG        origin/release/1.2.3
+#  STEP_TARGETS   build
+#)
+
 add_subdirectory(${CMAKE_SOURCE_DIR}/external/nlopt ${CMAKE_BINARY_DIR}/nlopt)
